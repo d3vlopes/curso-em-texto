4ea12fa8b788e98a5a2448dcb01c65c26a3282cf
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-08-18 20:13:30 -0300

    chore: add apps/backend test setup

--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 import baseConfig from '../../vitest.config';
 
 export default mergeConfig(
   baseConfig,
   defineConfig({
+    plugins: [tsconfigPaths()],
     test: {
       environment: 'node',
+      exclude: ['./src/__tests__/**'],
     },
   })
 );
