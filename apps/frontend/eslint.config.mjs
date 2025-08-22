0525ded6d89c673fb7f446115c0443f6a79425fb
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-08-22 19:17:17 -0300

    chore: config eslint plugins on frontend app

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -1,6 +1,11 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
 import baseConfig from '@curso-em-texto/eslint-config';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -23,8 +28,32 @@ const eslintConfig = [
     ],
   },
   {
-    plugins: {},
-    rules: {},
+    plugins: {
+      react,
+      'jsx-a11y': jsxA11y,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react-refresh/only-export-components': [
+        'off',
+        { allowExportNames: ['metadata'] },
+      ],
+    },
   },
 ];
 
