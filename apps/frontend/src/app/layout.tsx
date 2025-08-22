3e38cbd48e03fc7cd5373cadfc804d0ab31fca65
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-08-22 19:40:27 -0300

    feat: add frontend font family

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { Inter } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
+import '@/styles/globals.css';
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const inter = Inter({
+  variable: '--font-inter-sans',
+  weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
 });
 
@@ -23,12 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-BR">
+      <body className={`${inter.variable}  antialiased`}>{children}</body>
     </html>
   );
 }
