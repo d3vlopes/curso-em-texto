941fa9cbdfd79d3cf43231d33599a215214fff86
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-08-24 16:03:57 -0300

    build: remove eslint-plugin-react-refresh

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -5,7 +5,6 @@ import { FlatCompat } from '@eslint/eslintrc';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
-import reactRefresh from 'eslint-plugin-react-refresh';
 
 import baseConfig from '@curso-em-texto/eslint-config';
 
@@ -33,7 +32,6 @@ const eslintConfig = [
       react,
       'jsx-a11y': jsxA11y,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     languageOptions: {
       parserOptions: {
@@ -46,14 +44,6 @@ const eslintConfig = [
       'react/react-in-jsx-scope': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'react-refresh/only-export-components': [
-        'off',
-        { allowExportNames: ['metadata'] },
-      ],
     },
   },
   ...storybook.configs['flat/recommended'],
