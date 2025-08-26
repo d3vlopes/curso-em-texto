bbd9f0ba910886e4e046ea2235b0446a08800cf3
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-08-25 23:07:56 -0300

    fix: replace env for process.env on drizzle.config

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
 
-import { env } from './src/app/env';
-
 export default defineConfig({
   schema: './src/data/schema',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
