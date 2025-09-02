83e54f1188d4b02a4818c8bc135df43881263caf
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-09-02 15:33:16 -0300

    feat: add UserRepository

---
+++ apps/backend/src/data/repositories/interfaces/UserRepository.ts
@@ -0,0 +1,14 @@
+import type { UserModelData } from '@/data/models/User';
+
+export interface CreateUserData {
+  email: string;
+  name: string;
+  avatar?: string | null;
+  googleId?: string;
+  githubId?: string;
+  role?: string;
+}
+
+export interface UserRepository {
+  create(data: CreateUserData): Promise<UserModelData>;
+}
