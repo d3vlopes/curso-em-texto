26aa95078e6f6588f85879427e4d8bd20920b38a
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-09-10 20:04:32 -0300

    feat: add OAuthAdapter

--- apps/backend/src/app/adapters/http/fastify/fastify.d.ts
+++ apps/backend/src/app/adapters/http/fastify/fastify.d.ts
@@ -1,7 +1,13 @@
-import 'fastify';
-
+import type { OAuth2Namespace } from '@fastify/oauth2';
 declare module 'fastify' {
   interface FastifyRequest {
     userId?: string;
+  }
+}
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    googleOAuth2: OAuth2Namespace;
+    githubOAuth2: OAuth2Namespace;
   }
 }
