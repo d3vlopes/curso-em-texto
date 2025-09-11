51719f5a5140163bcc5f4a519ab34bb88c8c8573
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-09-11 14:02:12 -0300

    refactor: change declare module of fastify.d.ts

--- apps/backend/src/app/adapters/http/fastify/fastify.d.ts
+++ apps/backend/src/app/adapters/http/fastify/fastify.d.ts
@@ -1,11 +1,10 @@
 import type { OAuth2Namespace } from '@fastify/oauth2';
+
 declare module 'fastify' {
   interface FastifyRequest {
     userId?: string;
   }
-}
 
-declare module 'fastify' {
   interface FastifyInstance {
     googleOAuth2: OAuth2Namespace;
     githubOAuth2: OAuth2Namespace;
