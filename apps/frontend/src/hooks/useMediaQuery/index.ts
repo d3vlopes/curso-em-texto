b1c6bf51281742e5094eaa874fe4f90aa5807306
Leandro Lopes <new.leandrolopes@gmail.com>
2026-05-26 19:50:08 -0300

    refactor: optimize useMediaQuery hook initialization and listener

--- apps/frontend/src/hooks/useMediaQuery/index.ts
+++ apps/frontend/src/hooks/useMediaQuery/index.ts
@@ -1,21 +1,19 @@
 import { useEffect, useState } from 'react';
 
 export function useMediaQuery(query: string): boolean {
-  const [matches, setMatches] = useState(false);
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
 
   useEffect(() => {
     const media = window.matchMedia(query);
 
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
-
-    const listener = () => setMatches(media.matches);
+    const listener = (event: MediaQueryListEvent) => setMatches(event.matches);
 
     media.addEventListener('change', listener);
 
     return () => media.removeEventListener('change', listener);
-  }, [matches, query]);
+  }, [query]);
 
   return matches;
 }
