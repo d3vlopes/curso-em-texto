1d53a89b06b7b4cabaa91f2687571405cb2f0572
Leandro Lopes <new.leandrolopes@gmail.com>
2026-05-26 20:00:39 -0300

    build: upadate storybook

--- apps/frontend/.storybook/main.ts
+++ apps/frontend/.storybook/main.ts
@@ -1,26 +1,16 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
-import { join, dirname } from 'path';
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string) {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
-
 const config: StorybookConfig = {
   stories: ['../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-a11y'),
-    getAbsolutePath('@storybook/addon-vitest'),
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/nextjs-vite'),
+    name: '@storybook/nextjs-vite',
     options: {},
   },
-  staticDirs: ['../public'],
+  staticDirs: [],
 };
 export default config;
